9696447412d30338303703065f7011f25d7ddf8b
KevinTamCS <kevin@kevintamcs.com>
2020-07-14 01:46:37 -0400

    Move default unused files away

---
+++ unused/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../src/components/Navbar/Navbar';
+
+import Home from '../src/pages/Home';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
