37c5f9d2ed3e635046ab4c0c0e94ce3bccde69ef
KevinTamCS <kevin@kevintamcs.com>
2020-07-19 21:14:58 -0400

    Add desktop ending section of home page

--- src/pages/Home/components/Ending/Ending.jsx
+++ src/pages/Home/components/Ending/Ending.jsx
@@ -7,14 +7,32 @@ const Ending = () => {
   return (
     <section className="Ending">
       <div className="ending-container">
-        <h2>
-          Unlock your full potential at the Brooklyn College Computer Science
-          Club.
-        </h2>
+        <Header />
         <Link to="/join">Join the Club</Link>
       </div>
     </section>
   );
+};
+
+const Header = () => {
+  if (window.innerHeight > window.innerWidth) {
+    // Phone
+    return (
+      <h2>
+        Unlock your full potential at the Brooklyn College Computer Science
+        Club.
+      </h2>
+    );
+  } else {
+    // Desktop
+    return (
+      <h2>
+        Unlock your full potential at the
+        <br />
+        Brooklyn College Computer Science Club.
+      </h2>
+    );
+  }
 };
 
 export default Ending;
