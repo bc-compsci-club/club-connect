10af282dd405fc2ff56c9ecd23c81845a60b7036
KevinTamCS <kevin@kevintamcs.com>
2020-07-20 10:25:33 -0400

    Store new members in firebase

--- functions/handleJoin/handleJoin.js
+++ functions/handleJoin/handleJoin.js
@@ -1,6 +1,8 @@
 import querystring from 'querystring';
+import admin from 'firebase-admin';
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
+  // Only POST events are allowed
   console.log('Received event: ' + event);
   if (event.httpMethod !== 'POST') {
     return {
@@ -9,12 +11,27 @@ exports.handler = (event, context, callback) => {
     };
   }
 
-  const params = querystring.parse(event.body);
-  console.log(params);
+  // Initialize Firebase
+  admin.initializeApp({
+    credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT_KEY),
+  });
 
-  const firstName = params["first-name"];
-  const lastName = params["last-name"];
-  const email = params["email"];
+  // Initialize Firestore
+  const db = admin.firestore();
+
+  // Parse event body
+  const params = querystring.parse(event.body);
+  const firstName = params['first-name'];
+  const lastName = params['last-name'];
+  const email = params['email'];
+
+  // Create new Firestore document
+  const docRef = db.collection('members').doc(email);
+  await docRef.set({
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  });
 
   callback(null, {
     statusCode: 200,
