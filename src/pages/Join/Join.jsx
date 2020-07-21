7cfb9fc4aeda04546e63f9bf8ab6f9bc8fd0ba7e
KevinTamCS <kevin@kevintamcs.com>
2020-07-20 22:18:35 -0400

    Update form to use new endpoint, as well as styling changes

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,8 +1,35 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 import './Join.scss';
 
 const Join = () => {
+  const [submitValue, setSubmitValue] = useState('Join!');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const disableForm = () => {
+    setSubmitValue('Joining...');
+  };
+  /*   const disableForm = () => {
+    // Style submit button
+    document.getElementById('form-join-submit').disabled = true;
+    document.getElementById('form-join-submit').style.backgroundColor =
+    '#808cff';
+    document.getElementById('form-join-submit').style.boxShadow = 'none';
+    setSubmitValue('Joining...');
+
+    // Disable inputs
+    document.getElementById('first-name').disabled = true;
+    document.getElementById('last-name').disabled = true;
+    document.getElementById('email').disabled = true;
+
+    // Visual disabled input indicator
+    document.getElementById('first-name').style.color = '#4b4b4b';
+    document.getElementById('last-name').style.color = '#4b4b4b';
+    document.getElementById('email').style.color = '#4b4b4b';
+  }; */
+
   return (
     <section className="Join">
       <div className="join-flex-container">
@@ -10,29 +37,55 @@ const Join = () => {
           <h1>Join the Club</h1>
           <form
             name="join"
+            id="join-form"
             method="POST"
-            action="https://us-central1-bc-cs-club-website.cloudfunctions.net/handleJoin"
+            action="https://us-east4-bc-cs-club-website.cloudfunctions.net/handleJoin"
+            // onSubmit={disableForm}
           >
             <div className="form-name-row">
               <div className="form-first-name">
                 <label htmlFor="first-name">First Name*</label>
-                <input type="text" name="first-name" id="first-name" required />
+                <input
+                  className="form-join-field"
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
               </div>
 
               <div className="form-last-name">
                 <label htmlFor="last-name">Last Name*</label>
-                <input type="text" name="last-name" id="last-name" required />
+                <input
+                  className="form-join-field"
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
               </div>
             </div>
 
             <div className="form-email">
               <label htmlFor="email">Email Address*</label>
-              <input type="email" name="email" id="email" required />
+              <input
+                className="form-join-field"
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </div>
 
             <br />
             <div>
-              <input type="submit" value="Join!" />
+              <input id="form-join-submit" type="submit" value={submitValue} />
             </div>
           </form>
         </div>
