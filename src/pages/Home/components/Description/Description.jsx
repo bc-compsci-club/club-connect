7b0ff4178b5b5c06cd8034005cc68487efa5b904
KevinTamCS <kevin@kevintamcs.com>
2020-07-20 23:37:08 -0400

    Reduce image sizes

--- src/pages/Home/components/Description/Description.jsx
+++ src/pages/Home/components/Description/Description.jsx
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import './Description.scss';
-import image from 'assets/linkedin.png';
+import image from 'assets/linkedin.jpg';
 
 const Description = () => {
   return (
