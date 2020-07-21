9ac8e1179a2477bf78f7e4bdeb023bca76213e29
KevinTamCS <kevin@kevintamcs.com>
2020-07-20 22:25:37 -0400

    Disable submitValue

---
+++ src/pages/WelcomeJoin/WelcomeJoin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomeJoin = () => {
+  return (
+    <section className="WelcomeJoin">
+      <h1>You&amp;re in! Welcome to the club!</h1>
+    </section>
+  );
+};
+
+export default WelcomeJoin;
