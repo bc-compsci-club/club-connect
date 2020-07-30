1ae8c5740c684b3e584a777a3c81c25baa283b3e
KevinTamCS <kevin@kevintamcs.com>
2020-07-30 12:51:36 -0400

    Add top of about section

--- src/pages/About/components/Top/Top.jsx
+++ src/pages/About/components/Top/Top.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Top.scss';
+// import mobileHeader from 'assets/about/about-header-mobile.jpg';
 
 const Top = () => {
   return (
     <section className="about-top">
-      <h1>Together, we make magic happen.</h1>
-      <Link to="/join">Join the Club</Link>
+      {/*<img src={mobileHeader} alt="Header"/>*/}
+      <div className="about-top-container">
+        <h1>Together, we make magic happen.</h1>
+        <Link to="/join" className="about-join-button">Join the Club</Link>
+      </div>
     </section>
   );
 };
