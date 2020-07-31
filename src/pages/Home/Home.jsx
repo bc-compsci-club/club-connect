6014f06dbae53b92577dfcfae218b414497c6cf2
KevinTamCS <kevin@kevintamcs.com>
2020-07-31 00:29:10 -0400

    Make Home page more compatible with Reader Mode

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import Welcome from './components/Welcome';
 import Description from './components/Description';
 import Highlights from './components/Highlights';
-import Ending from './components/Ending';
+import Ending from '../../components/Ending';
 
 const Home = () => {
   return (
     <div className="Home">
-      <Welcome />
-      <Description />
-      <Highlights />
-      <Ending />
+      <article>
+        <Welcome />
+        <Description />
+        <Highlights />
+        <Ending />
+      </article>
     </div>
   );
 };
