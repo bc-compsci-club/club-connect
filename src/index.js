006ae79ecc93362f3323c5d59756ee39134e0ed0
KevinTamCS <kevin@kevintamcs.com>
2020-08-03 20:11:04 -0400

    Enable react-app-polyfill

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // @flow
-import React from 'react';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import React  from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
