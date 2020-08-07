5363e31fd8a5d2d0631d57c19aedc1e3f9b70fbf
KevinTamCS <kevin@kevintamcs.com>
2020-08-07 15:18:46 -0400

    Move iOS Utilities to its own file

--- src/components/Navbar/NavbarHamburgerMenu/NavbarHamburgerMenuContext/NavbarHamburgerMenuContext.jsx
+++ src/components/Navbar/NavbarHamburgerMenu/NavbarHamburgerMenuContext/NavbarHamburgerMenuContext.jsx
@@ -1,23 +1,9 @@
 // @flow
 import React, { useEffect, useState } from 'react';
+import { isUsingiOS, getiOSVersion } from 'utils/iOSUtils';
 
 // make a new context
 const NavbarHamburgerMenuContext = React.createContext();
-
-// Checks if the device is an iOS device.
-const isUsingiOS = () => {
-  return /iP(hone|od touch|ad)/.test(navigator.platform);
-};
-
-// Gets the iOS version. Returns null if the device isn't an iOS device.
-const getiOSVersion = () => {
-  if (/iP(hone|od touch|ad)/.test(navigator.platform)) {
-    const version = navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
-    return parseInt(version[1], 10);
-  }
-
-  return null;
-};
 
 // create the provider
 const NavbarHamburgerMenuProvider = (props) => {
@@ -27,11 +13,15 @@ const NavbarHamburgerMenuProvider = (props) => {
   useEffect(() => {
     if (isUsingiOS() && getiOSVersion() < 9) {
       // Disable the sliding animation
-      document.getElementsByClassName('bm-menu-wrap')[0].style.WebkitTransitionDuration = '0';
+      document.getElementsByClassName(
+        'bm-menu-wrap'
+      )[0].style.WebkitTransitionDuration = '0';
 
       // Hide both the menu and the overlay
-      document.getElementsByClassName('bm-menu-wrap')[0].style.WebkitTransform = 'translate3d(100%, 0, 0)';
-      document.getElementsByClassName('bm-overlay')[0].style.WebkitTransform = 'translate3d(100%, 0, 0)';
+      document.getElementsByClassName('bm-menu-wrap')[0].style.WebkitTransform =
+        'translate3d(100%, 0, 0)';
+      document.getElementsByClassName('bm-overlay')[0].style.WebkitTransform =
+        'translate3d(100%, 0, 0)';
     }
   }, []);
 
