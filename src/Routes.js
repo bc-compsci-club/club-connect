43f6596b6945bbc6a5f9b5487207c116fa079cf9
Restyled.io <commits@restyled.io>
2020-08-09 21:07:27 +0000

    Restyled by clang-format

--- src/Routes.js
+++ src/Routes.js
@@ -1,36 +1,28 @@
 // @flow
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from 'pages/Home';
 import About from 'pages/About';
 import Events from 'pages/Events';
-import Resources from 'pages/Resources';
+import Home from 'pages/Home';
 import Join from 'pages/Join';
+import Resources from 'pages/Resources';
 import WelcomeJoin from 'pages/WelcomeJoin';
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 const Routes = () => {
-  return (
-    <Switch>
-      <Route path="/welcome">
-        <WelcomeJoin />
-      </Route>
-      <Route path="/join">
-        <Join />
-      </Route>
-      <Route path="/resources">
-        <Resources />
-      </Route>
-      <Route path="/events">
-        <Events />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
+  return (<Switch><Route path = "/welcome"><WelcomeJoin />
+          </Route>
+      <Route path="/join ">
+           < Join /></Route>
+      <Route path="/resources ">
+                     < Resources /></Route>
+      <Route path="/events ">
+                                    < Events />
+          </Route>
+      <Route path="/about ">
+           < About /></Route>
       <Route exact path="/">
-        <Home />
-      </Route>
-    </Switch>
-  );
+                      < Home /></Route>
+    </Switch>);
 };
 
 export default Routes;
