5ce0f1783482dae1a7ff2b563d7a5da9f7fd819d
KevinTamCS <kevin@kevintamcs.com>
2020-08-10 02:40:22 -0400

    Add noopener to link

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -8,13 +8,19 @@ const Events = () => {
       <h1>Upcoming Events</h1>
       <ul className="events-list">
         <li>
-          <Link to="/events/1/meet-the-board">Meet the Board: September 10</Link>
+          <Link to="/events/1/meet-the-board">
+            Meet the Board: September 10
+          </Link>
         </li>
         <li>
-          <Link to="/events/2/donuts-and-discussion">Donuts and Discussion: September 16</Link>
+          <Link to="/events/2/donuts-and-discussion">
+            Donuts and Discussion: September 16
+          </Link>
         </li>
         <li>
-          <Link to="/events/3/facul-tea-time">Facul-Tea Time: September 22</Link>
+          <Link to="/events/3/facul-tea-time">
+            Facul-Tea Time: September 22
+          </Link>
         </li>
       </ul>
       <span role="img" aria-label="Events Notepad/Calendar">
@@ -25,7 +31,10 @@ const Events = () => {
         A new and improved event browser will be available soon! If you want to
         help us build it (and get some experience contributing to open source as
         well!), feel free to visit the{' '}
-        <a href="https://github.com/bc-compsci-club/bccompsci.club">
+        <a
+          href="https://github.com/bc-compsci-club/bccompsci.club"
+          rel="noopener"
+        >
           website's GitHub repository
         </a>{' '}
         for more details on how to contribute and to create a pull request.
