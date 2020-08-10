9eb6f78c1eaffc532313a50fd7fd94c03a9b9b0a
KevinTamCS <kevin@kevintamcs.com>
2020-08-10 02:02:36 -0400

    Add dekstop styles for Event page

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import './Events.scss';
+import { Link } from 'react-router-dom';
 
 const Events = () => {
   return (
     <div className="Events">
       <h1>Upcoming Events</h1>
-      <span role="img"
-            aria-label="Events Notepad/Calendar">🗒</span>
-      <h2>Coming soon...</h2>
+      <ul className="events-list">
+        <li>
+          <Link to="/events/1/meet-the-board">Meet the Board: September 10</Link>
+        </li>
+        <li>
+          <Link to="/events/2/donuts-and-discussion">Donuts and Discussion: September 16</Link>
+        </li>
+        <li>
+          <Link to="/events/3/facul-tea-time">Facul-Tea Time: September 22</Link>
+        </li>
+      </ul>
+      <span role="img" aria-label="Events Notepad/Calendar">
+        🗒
+      </span>
+      <h2>New Event Browser coming soon...</h2>
       <p>
-        We&apos;ve got some awesome events planned for our club members once the
-        semester starts! Check back here around mid-August for more details.
-        We&apos;ll be notifying our club members in our monthly newsletter once
-        the events are up. Make sure to join the club before then so you
-        don&apos;t miss out! See you soon at an event!
+        A new and improved event browser will be available soon! If you want to
+        help us build it (and get some experience contributing to open source as
+        well!), feel free to visit the{' '}
+        <a href="https://github.com/bc-compsci-club/bccompsci.club">
+          website's GitHub repository
+        </a>{' '}
+        for more details on how to contribute and to create a pull request.
       </p>
     </div>
   );
