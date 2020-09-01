a53f108a55229c55557cd4b204d726a676564563
KevinTamCS <kevin@kevintamcs.com>
2020-09-01 16:18:33 -0400

    Overhaul Events page

---
+++ src/pages/Events/components/Event.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Event.scss';
+
+const Event = (props) => {
+  return (
+    <div className="Events-event">
+      <Link to={`events/${props.id}/${props.name}`}>
+        <img
+          src={`${process.env.PUBLIC_URL}/data/events/${props.id}-${props.name}/${props.name}-banner.png`}
+          alt="Event Banner"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Event;
