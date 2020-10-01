85022827034fb396f373cd63e7fa0cbf5f066599
KevinTamCS <kevin@kevintamcs.com>
2020-10-01 18:31:03 -0400

    Remove helmet import

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import { Helmet } from 'react-helmet';
 import Event from './components/Event';
 import '../page-styles.scss';
 import './Events.scss';
