9bfdc002617da31c871b46fa5ea9f5fdd821bfc3
KevinTamCS <kevin@kevintamcs.com>
2021-01-17 16:39:29 -0500

    Add Next.js MDX support

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,15 @@
-const withImages = require('next-images');
-
-module.exports = withImages({
-  distDir: 'build',
+const withPlugins = require('next-compose-plugins');
+const images = require('next-images');
+const mdx = require('@next/mdx')({
+  extension: /\.mdx?$/,
 });
+
+const nextConfig = {
+  distDir: 'build',
+};
+
+const mdxConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+};
+
+module.exports = withPlugins([images, [mdx, mdxConfig]], nextConfig);
