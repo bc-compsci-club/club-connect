fd20ba9349c7c68d3e39b72fa30622ba400209d1
KevinTamCS <kevin@kevintamcs.com>
2021-01-17 19:45:09 -0500

    Remove unused component

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -2,4 +2,3 @@ export { default as Ending } from './Ending';
 export { default as Footer } from './Footer';
 export { default as HamburgerMenu } from './HamburgerMenu';
 export { default as Header } from './Header';
-export { default as TypingEffect } from './TypingEffect';
