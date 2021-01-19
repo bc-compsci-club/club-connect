46d6b5c847a385b75dcb3b41fab43f6c40640465
KevinTamCS <kevin@kevintamcs.com>
2021-01-18 21:42:13 -0500

    Move head back to _app.jsx
    https://github.com/vercel/next.js/blob/master/errors/no-document-viewport-meta.md

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -64,6 +64,30 @@ const MyApp = ({ Component, pageProps }) => {
 
   return (
     <>
+      <Head>
+        <title>
+          Brooklyn College Computer Science Club | Brooklyn College's Premier
+          Computer Science Community
+        </title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* IE11 Custom Properties Polyfill Script */}
+        {/* Will only load if IE11 is being used */}
+        {typeof window !== 'undefined' &&
+          window.MSInputMethodContext &&
+          document.documentMode &&
+          console.log('Polyfilling IE11 Custom Properties...') &&
+          document.write(
+            '<script src="https://cdn.jsdelivr.net/gh/nuxodin/ie11CustomProperties@4.1.0/ie11CustomProperties.min.js"></script>'
+          )}
+      </Head>
       <HamburgerMenuProvider>
         <HamburgerMenu />
         <Header />
