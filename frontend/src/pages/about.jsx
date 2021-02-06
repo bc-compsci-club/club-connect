5062a2025950be362363f3179f7009cf5224747a
KevinTamCS <kevin@kevintamcs.com>
2021-02-05 23:56:00 -0500

    Update About page

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 import Head from 'next/head';
 
 import {
-  TopSection,
-  WhoWeAreSection,
-  WhatWeDoSection,
   TeamSection,
+  TopSection,
+  WhatWeDoSection,
+  WhoWeAreSection,
 } from 'components/about';
 import { Ending } from 'components/common';
-import styles from 'styles/pages/About.module.scss';
 import { SITE_TITLE_BASE } from 'pages/_app';
 
 const About = (props) => {
   return (
     <>
       <Head>
-        <title>About | {SITE_TITLE_BASE}</title>
+        <title>About Us | {SITE_TITLE_BASE}</title>
       </Head>
-      <div className={styles.about}>
+      <div>
         <TopSection />
         <WhoWeAreSection />
         <WhatWeDoSection />
