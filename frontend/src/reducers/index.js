399389185cb2be2fa178f1ae8e94bf71c879f251
KevinTamCS <kevin@kevintamcs.com>
2021-02-06 00:08:05 -0500

    Add Redux reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
 import userLoggedInReducer from 'reducers/userLoggedIn';
+import dropdownOpenReducer from 'reducers/dropdownOpen';
 
-const reducers = combineReducers({ user: userLoggedInReducer });
+const reducers = combineReducers({
+  userLoggedIn: userLoggedInReducer,
+  dropdownOpen: dropdownOpenReducer,
+});
 
 export default reducers;
