dfd3a107512fc40e2a1197f2cb75cd56c3913311
KevinTamCS <kevin@kevintamcs.com>
2021-02-06 00:17:01 -0500

    Upgrade root route

--- backend/src/routes/root.ts
+++ backend/src/routes/root.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const rootRouter = Router();
 
-rootRouter.get('/', (req, res) => {
-  res.send('Hello World!');
+/**
+ * Base route that returns 200 OK, mainly used to check if the backend is working
+ *
+ * @route GET /
+ */
+rootRouter.get('/', (req: Request, res: Response) => {
+  res.sendStatus(200);
 });
 
 export default rootRouter;
