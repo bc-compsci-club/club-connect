ddd9364dae85c8d0f4a205922a10d01f48d36ab7
KevinTamCS <kevin@kevintamcs.com>
2021-02-07 13:39:38 -0500

    Modularize footer styles and combine div styles

--- frontend/src/components/common/Footer/Footer.jsx
+++ frontend/src/components/common/Footer/Footer.jsx
@@ -3,31 +3,35 @@ import React from 'react';
 import styles from './Footer.module.scss';
 
 const Footer = (props) => {
+  const { width } = props;
+
   return (
     <footer className={styles.footer}>
-      {props.width < 700 ? (
-        // < 700px (Vertical phones and small horizontal phones)
-        <div className={styles.footerContainer}>
-          <p>© 2021 Brooklyn College Computer Science Club</p>
-          <p>0317 Ingersoll Hall, Brooklyn College</p>
-          <p>
-            <a href="mailto:contact@bccompsci.club">
-              Email: contact@bccompsci.club
-            </a>
-          </p>
-        </div>
-      ) : (
-        // > 700px (Desktops, laptops, tablets, and large horizontal phones)
-        <div className={styles.footerContainer}>
-          <p>© 2021 Brooklyn College Computer Science Club</p>
-          <p>
-            0317 Ingersoll Hall, Brooklyn College •{' '}
-            <a href="mailto:contact@bccompsci.club">
-              Email: contact@bccompsci.club
-            </a>
-          </p>
-        </div>
-      )}
+      <div className={styles.footerContainer}>
+        {width < 700 ? (
+          // < 700px (Vertical phones and small horizontal phones)
+          <>
+            <p>© 2021 Brooklyn College Computer Science Club</p>
+            <p>0317 Ingersoll Hall, Brooklyn College</p>
+            <p>
+              <a href="mailto:contact@bccompsci.club">
+                Email: contact@bccompsci.club
+              </a>
+            </p>
+          </>
+        ) : (
+          // > 700px (Desktops, laptops, tablets, and large horizontal phones)
+          <>
+            <p>© 2021 Brooklyn College Computer Science Club</p>
+            <p>
+              0317 Ingersoll Hall, Brooklyn College •{' '}
+              <a href="mailto:contact@bccompsci.club">
+                Email: contact@bccompsci.club
+              </a>
+            </p>
+          </>
+        )}
+      </div>
     </footer>
   );
 };
