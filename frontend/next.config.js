e05aefc3763588766a0a08aace61fd8e9f27ef5e
Brooklyn College Computer Science Club <53794908+BC-CompSciClub@users.noreply.github.com>
2021-09-22 18:54:42 -0400

    Commented out a block

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,9 +4,9 @@ const mdx = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-const nextConfig = {
-  distDir: 'build',
-};
+// const nextConfig = {
+//   distDir: 'build',
+// };
 
 const mdxConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
