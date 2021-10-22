7fd12f33aed63992b60e0c578bab5a07103a04d6
Brooklyn College Computer Science Club <53794908+BC-CompSciClub@users.noreply.github.com>
2021-10-22 15:27:49 -0400

    Update Ending.jsx
    link to google doc added for join button

--- frontend/src/components/common/Ending/Ending.jsx
+++ frontend/src/components/common/Ending/Ending.jsx
@@ -8,7 +8,7 @@ const Ending = ({ width }) => {
     <section className={styles.ending}>
       <div className={styles.endingContainer}>
         <EndingText width={width} />
-        <Link href="/join">Join the Club</Link>
+        <Link href = "https://docs.google.com/forms/d/e/1FAIpQLScb5bZEYE9sBzT57bbepmSFy6M21yoOWg4i5zHYSicfkxTOOg/viewform?usp=sf_link" target="_blank" >Join the Club</Link>
       </div>
     </section>
   );
